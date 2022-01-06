4ad8bf76c922f7f052459bdedbc9d2ebde8b4108
shakti2000 <74964959+shakti2000@users.noreply.github.com>
2022-01-06 15:12:19 +0530

    Time: 0 ms (100.00%), Space: 6 MB (30.31%) - LeetHub

--- 1025-divisor-game/1025-divisor-game.cpp
+++ 1025-divisor-game/1025-divisor-game.cpp
@@ -7,10 +7,13 @@ public:
         if(dp[n]!=-1)return dp[n];
         for(int i=1;i*i<=n;i++){
             if(n%i==0){
-            if(help(n-i)==0)return dp[n]=1;
+            if(help(n-i)==0){
+                dp[n]=1;
+                return 1;}
         }
     }
-    return dp[n]=0;
+        dp[n]=0;
+    return 0;
 }
     bool divisorGame(int n) {
     memset(dp,-1,sizeof dp);
