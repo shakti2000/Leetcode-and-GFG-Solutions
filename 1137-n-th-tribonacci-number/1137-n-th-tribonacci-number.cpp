8f13bb9ffbc25d57759f2f90eb567d71a670e077
shakti2000 <74964959+shakti2000@users.noreply.github.com>
2022-01-07 11:20:04 +0530

    Time: 2 ms (12.40%), Space: 6 MB (29.35%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -1,18 +1,30 @@
 class Solution {
 public:
-    map<int,int>shakti;
-    int checker(int n){
-           if( shakti.count(n) != 0) return shakti.at(n);
-        int ans = checker(n-1) + checker(n-2) + checker(n-3);
-        shakti[n] = ans;
-        return ans; 
-        }
+    
     int tribonacci(int n) {
-     if(n==0)return n;
+    vector<int>shakti(n+1,0);
+        if(n==0){
+            return 0;
+        }
+        if(n==2){
+            int x=1;
+            return x;
+        }
+        
+        if(n==1){
+            int x=1;
+            return x;
+        }
         shakti[0]=0;
         shakti[1]=1;
         shakti[2]=1;
-        
-        return checker(n); 
+        for(int i=3;i<=n;i++){
+            if(shakti[i]!=0)return shakti[i];
+            else{
+                int ans=shakti[i-1]+shakti[i-2]+shakti[i-3];
+                shakti[i]=ans;
+            }
+        }
+    return shakti[n];
     }
 };
