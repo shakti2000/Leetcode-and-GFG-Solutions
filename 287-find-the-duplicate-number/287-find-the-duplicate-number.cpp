ca75812a381fd92168c06d8c048068a4ac7e2a66
shakti2000 <74964959+shakti2000@users.noreply.github.com>
2022-01-10 12:02:44 +0530

    Time: 96 ms (87.58%), Space: 61.2 MB (58.86%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
@@ -1,11 +1,25 @@
 class Solution {
 public:
-    int findDuplicate(vector<int>& nums) {
-        unordered_set<int>s;
-        for(int i=0; i<nums.size();i++){
-            if(s.count(nums[i]))return nums[i];
-            s.insert(nums[i]);
+    int findDuplicate(vector<int>& arr){
+        int n = arr.size();
+		
+		
+        // 1 --> piegeonhole principle
+        for(int i=0;i<n;i++){
+            if(arr[abs(arr[i])] < 0) return abs(arr[i]);
+            else arr[abs(arr[i])] *= -1; 
         }
-        return 0;
+        // -----------------------------------------------------------
+		
+        //2 --> normal sum -- as the test cases are faulty so this approach will give wrong ans
+        int sum = 0;
+        for(auto it : arr){
+            sum += it;
+        }
+        int tn = n-1;
+        int orgSum = (tn * (tn + 1)) / 2;
+        int ans = sum - orgSum;
+        return ans;
+		// ----------------------------------------------------
     }
 };
