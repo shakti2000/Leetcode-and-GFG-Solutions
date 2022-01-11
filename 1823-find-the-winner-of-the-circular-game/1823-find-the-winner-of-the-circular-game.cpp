f483355b25e8e6eee9824a888dcc8747e33e2dae
shakti2000 <74964959+shakti2000@users.noreply.github.com>
2022-01-11 13:17:31 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (90.09%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
@@ -1,19 +1,10 @@
 class Solution {
 public:
     int findTheWinner(int n, int k) {
-        list<int>l;
-        for(int i=0;i<n;i++){
-            l.push_back(i);
+        int res=0;
+        for(int i=1; i<=n; i++) {
+            res = (res+k)%i;
         }
-        auto it=l.begin();
-        while(l.size()>1){
-            for(int i=1;i<k;i++){
-                it++;
-                if(it==l.end())it=l.begin();
-            }
-            it=l.erase(it);
-            if(it==l.end())it=l.begin();
-        }
-    return 1+*l.begin();
+        return res+1;
     }
 };
