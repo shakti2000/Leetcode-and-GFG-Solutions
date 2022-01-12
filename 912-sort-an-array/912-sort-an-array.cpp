6e1f21d1be4992b0e587e6b00869d587773e320b
shakti2000 <74964959+shakti2000@users.noreply.github.com>
2022-01-12 13:14:59 +0530

    Time: 113 ms (32.49%), Space: 28.6 MB (46.05%) - LeetHub

--- 912-sort-an-array/912-sort-an-array.cpp
+++ 912-sort-an-array/912-sort-an-array.cpp
@@ -1,7 +1,44 @@
 class Solution {
 public:
+    void merge (vector<int>&nums,int l,int m,int r){
+    int n1 = m - l + 1;
+    int n2 = r - m;
+    int A[n1], B[n2];
+    
+    for(int i = 0; i < n1; i++)
+        A[i] = nums[l + i];
+    
+    for(int i = 0; i < n2; i++)
+        B[i] = nums[m + 1 + i];
+    
+    int i = 0, j = 0;
+    int k = l;
+    
+    while(i < n1 && j < n2)
+    {
+        if(A[i] <= B[j])
+            nums[k++] = A[i++];
+        else
+            nums[k++] = B[j++];
+    }
+    
+    while(i < n1)
+        nums[k++] = A[i++];
+    while(j < n2)
+        nums[k++] = B[j++];
+}
+    void mergesort(vector<int>&arr,int l,int r){
+      if(r>l){
+          int m=l+(r-l)/2;
+          mergesort(arr,l,m);
+          mergesort(arr,m+1,r);
+          merge(arr,l,m,r);
+      }  
+    }
     vector<int> sortArray(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
+        int l=0;
+        int r=nums.size()-1;
+        mergesort(nums,l,r);
         return nums;
     }
 };
