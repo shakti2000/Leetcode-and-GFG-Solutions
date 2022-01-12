7b1254f6aa85a77878de4fcc53c84942fc158efa
shakti2000 <74964959+shakti2000@users.noreply.github.com>
2022-01-12 10:17:05 +0530

    Added solution - LeetHub

---
+++ Check if subtree - GFG/check-if-subtree.cpp
@@ -0,0 +1,160 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// Tree Node
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+// Function to Build Tree
+Node *buildTree(string str) {
+    // Corner Case
+    if (str.length() == 0 || str[0] == 'N')
+        return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for (string str; iss >> str;)
+        ip.push_back(str);
+
+    // Create the root of the tree
+    Node *root = new Node(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node *> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while (!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node *currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if (currVal != "N") {
+
+            // Create the left child for the current node
+            currNode->left = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= ip.size())
+            break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if (currVal != "N") {
+
+            // Create the right child for the current node
+            currNode->right = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+
+ // } Driver Code Ends
+/* A binary tree node
+
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};
+ */
+
+class Solution
+{
+  public:
+    void low(Node* root,vector<int>& a)
+    {
+        if(root!=NULL)
+        {
+            a.push_back(root->data);
+            if(root->left==NULL)
+            a.push_back(0);
+            if(root->right==NULL)
+            a.push_back(0);
+            low(root->left,a);
+            low(root->right,a);
+        }
+    }
+    bool isSubTree(Node* T, Node* S) 
+    {
+        vector<int> a,b;
+        low(T,a);
+        low(S,b);
+        int j=0;
+        for(int i=0;i<a.size();i++)
+        {
+            if(a[i]==b[j])
+            {
+                int k=i+1;
+                j++;
+                while(j<b.size()&&k<a.size())
+                {
+                    if(a[k]==b[j])
+                    {
+                        j++;
+                        k++;
+                    }
+                    else
+                    break;
+                }
+                if(j==b.size())
+                return 1;
+                j=0;
+            }
+        }
+        return 0;
+    }
+};
+
+// { Driver Code Starts.
+
+int main() {
+    int tc;
+    scanf("%d ", &tc);
+    while (tc--) {
+        string strT, strS;
+        getline(cin, strT);
+        Node *rootT = buildTree(strT);
+        getline(cin, strS);
+        Solution obj;
+        Node *rootS = buildTree(strS);
+        cout << obj.isSubTree(rootT, rootS) << "\n";
+
+    }
+    return 0;
+}  // } Driver Code Ends
