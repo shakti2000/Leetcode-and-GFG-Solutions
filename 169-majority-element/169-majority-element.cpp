da5a2c9eff1d9c2717a1e515d3a79d46854679a2
Shakti Aggarwal <74964959+shakti2000@users.noreply.github.com>
2022-01-21 15:52:40 +0530

    Time: 16 ms (86.78%), Space: 19.7 MB (50.43%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -3,7 +3,7 @@ public:
     int majorityElement(vector<int>& nums) {
         int can=nums[0];
         int count=1;
-        for(int i=0;i<nums.size();i++){
+        for(int i=1;i<nums.size();i++){
             if(nums[i]==can){
                 count+=1;
             }else{
