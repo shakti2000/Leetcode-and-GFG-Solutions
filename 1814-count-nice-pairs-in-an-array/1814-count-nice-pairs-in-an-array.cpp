4f71178ca9c6d070d5171af22f89a4fb1faecca4
Shakti Aggarwal <74964959+shakti2000@users.noreply.github.com>
2022-01-21 23:14:44 +0530

    Time: 202 ms (35.79%), Space: 56.8 MB (86.55%) - LeetHub

--- 1814-count-nice-pairs-in-an-array/1814-count-nice-pairs-in-an-array.cpp
+++ 1814-count-nice-pairs-in-an-array/1814-count-nice-pairs-in-an-array.cpp
@@ -1,32 +1,38 @@
 class Solution {
 public:
-    long long mod=1000000007;
-    int reve(int x){
-        //cout<<x;
-        stack<int>rev;
-        while(x>0){
-            rev.push(x%10);
-            x/=10;
+        long long mod = 1e9 + 7;
+    
+    int reverse(int n)
+    {
+        int rev = 0;
+        
+        while(n > 0)
+        {
+            rev = rev * 10 + n % 10;
+            n = n / 10;
         }
-        long long m=1;
-        int ans=0;
-        while(!rev.empty()){
-            int temp=rev.top()*m;
-            ans+=temp;
-            m*=10;
-            rev.pop();
-        }
-      return ans;
+        
+        return rev;
     }
-    int countNicePairs(vector<int>& nums) {
-        unordered_map<int,int>mp;
-        int ans=0;
-        for(int i=0;i<nums.size();i++){
-           
-              int temp=nums[i]-reve(nums[i]);
-              if(mp[temp]!=0){ans=(ans%mod+mp[temp]%mod)%mod;}
-              mp[temp]++;
-        }
-    return ans;
+    
+    int countNicePairs(vector<int>& arr) {
+        
+        int n = arr.size();
+        
+        int count = 0;
+        
+        unordered_map<int, int> mp;
+        
+        for(int i = 0; i < n; i++)
+        {
+            int x = arr[i] - reverse(arr[i]);
+            
+            if(mp.count(x))
+            {
+                count = (count % mod + (mp[x]) % mod) % mod;
+            }   
+            mp[x]++;
+        }   
+        return count++;
     }
 };
