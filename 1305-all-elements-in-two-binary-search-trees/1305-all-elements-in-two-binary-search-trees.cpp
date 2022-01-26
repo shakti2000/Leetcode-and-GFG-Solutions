18f00d81c8617acd6bd5959d70856b063265ee2e
Shakti Aggarwal <74964959+shakti2000@users.noreply.github.com>
2022-01-26 09:29:18 +0530

    Time: 136 ms (75.44%), Space: 84.3 MB (97.28%) - LeetHub

--- 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.cpp
+++ 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.cpp
@@ -11,39 +11,18 @@
  */
 class Solution {
 public:
-    void calc(TreeNode* root,vector<int>&a){
+    void inorder(TreeNode*root,vector<int>&ans){
         if(root==NULL)return;
-        calc(root->left,a);
-        a.push_back(root->val);
-        calc(root->right,a);
-    }
-    vector<int> merge(vector<int>&a,vector<int>&b){
-        vector<int>ans;
-        int i=0;
-        int j=0;
-        while(i<a.size()&&j<b.size()){
-            if(a[i]<b[j]){
-                ans.push_back(a[i]);
-                i++;
-            }else{
-                ans.push_back(b[j]);
-                j++;
-            }
-        }
-        if(i==a.size()){
-            for(int x=j;x<b.size();x++)ans.push_back(b[x]);
-            return ans;
-        }else{
-            for(int x=i;x<a.size();x++)ans.push_back(a[x]);
-            return ans;
-        }
+        inorder(root->left,ans);
+        ans.push_back(root->val);
+        inorder(root->right,ans);
+        return;
     }
     vector<int> getAllElements(TreeNode* root1, TreeNode* root2) {
-        vector<int>a;
-        vector<int>b;
-        calc(root1,a);
-        calc(root2,b);
-        vector<int>ans=merge(a,b);
-    return ans;
-    }
+        vector<int >ans;
+        inorder(root1,ans);
+        inorder(root2,ans);
+        sort(ans.begin(),ans.end());
+        
+    return ans;}
 };
