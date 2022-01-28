b4d639d3b1a49374dce592980b04c5d0665ce5e1
Shakti Aggarwal <74964959+shakti2000@users.noreply.github.com>
2022-01-28 22:11:05 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (66.22%) - LeetHub

---
+++ 78-subsets/78-subsets.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> powerSet;
+        
+        for(int mask = 0; mask < (1<< nums.size()); mask++){
+            vector<int> subset;
+            for(int i = 0; i < nums.size(); i++ ){                
+                if(mask & (1 << i)){
+                    subset.push_back(nums[i]);
+                }
+            }            
+            powerSet.push_back(subset);
+            
+        }
+        return powerSet;
+    }
+};
