6943130a4c565e1bd72b6920753bb0b6f15151ab
Shakti Aggarwal <74964959+shakti2000@users.noreply.github.com>
2022-01-28 23:46:43 +0530

    Time: 274 ms (5.02%), Space: 23.4 MB (34.32%) - LeetHub

--- 187-repeated-dna-sequences/187-repeated-dna-sequences.cpp
+++ 187-repeated-dna-sequences/187-repeated-dna-sequences.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
-    vector<string> findRepeatedDnaSequences(string s) 
-    {
-        int i = 0, j = 0, k = 10;
-        unordered_map<string, int> freq;
+    vector<string> findRepeatedDnaSequences(string s) {
+        unordered_map<string,int>freq;
         vector<string>ans;
-        while(j<s.size())
-        {
-            if(j - i +1 == k)
-            {
-                string sub = s.substr(i, k);
-                freq[sub]++;
-                if(freq[sub]  == 2)
-                    ans.push_back(sub);
-                i++;
-            }
-            j++;
+        string x=s.substr(0,10);
+        //cout<<x;
+        freq[x]++;
+        for(int i=10;i<s.length();i++){
+            string x=s.substr((i-9),10);
+           cout<<x<<endl;
+            //cout<<freq[x]<<",";
+            freq[x]++;           
+            
+                if(freq[x]==2)
+                ans.push_back(x);
         }
+        
         return ans;
     }
 };
