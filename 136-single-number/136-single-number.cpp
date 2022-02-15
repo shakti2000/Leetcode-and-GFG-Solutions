a40a2b5190efe107eae737aae4bb006865134b58
Shakti Aggarwal <74964959+shakti2000@users.noreply.github.com>
2022-02-15 16:27:00 +0530

    Time: 20 ms (76.93%), Space: 16.9 MB (50.83%) - LeetHub

---
+++ 136-single-number/136-single-number.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int temp=0;
+        for(int i=0;i<nums.size();i++){
+            temp^=nums[i];
+        }
+    return temp;}
+};
