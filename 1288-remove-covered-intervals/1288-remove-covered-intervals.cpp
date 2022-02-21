e221d31a6bea3e2ba9962ed4fcda50835fec212f
Shakti Aggarwal <74964959+shakti2000@users.noreply.github.com>
2022-02-21 14:32:33 +0530

    Time: 119 ms (13.88%), Space: 28.6 MB (11.71%) - LeetHub

--- 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
@@ -1,18 +1,41 @@
 class Solution {
 public:
-    int removeCoveredIntervals(vector<vector<int>>& intervals) {
-        sort(intervals.begin(), intervals.end(), comparator);
-        int cur = 0;
-        int remaining = intervals.size();
-        for (int i = 1; i < intervals.size(); ++i) {
-            if (intervals[i][1] <= intervals[cur][1]) --remaining;
-            else cur = i;
+    void print(vector<vector<int>>&x){
+        for(auto it:x){
+            for(auto i:it){
+                cout<<i<<",";
+            }
+            cout<<endl;
         }
-        return remaining;
     }
-    
-    static bool comparator(vector<int>& int1, vector<int>& int2) {
-        if (int1[0] == int2[0]) return int1[1] > int2[1];
-        else return int1[0] < int2[0];
+    static bool shakti(vector<int>a,vector<int>b){
+        if(a[0]!=b[0])return a[0]<b[0];
+        else{
+            return a[1]>b[1];
+        }
     }
+    int removeCoveredIntervals(vector<vector<int>>& interval) {
+        sort(interval.begin(),interval.end(),shakti);
+        //print(interval);
+        int start=interval[0][0];
+        int end=interval[0][1];
+        int ans=1;
+        for(int i=0;i<interval.size();i++){
+            if(interval[i][0]>=start&&interval[i][1]<=end)continue;
+            if(interval[i][0]>end){
+                start=interval[i][0];
+                end=interval[i][1];
+                ans++;
+                continue;
+                //cout<<"yha ans++"<<ans<<endl;
+            }
+            if(interval[i][1]>end){
+                //cout<<end<<"se";
+                end=interval[i][1];
+                ans++;
+                //cout<<end<<"step"<<i<<"mein"<<endl;
+                continue;
+            }
+        }
+    return ans;}
 };
