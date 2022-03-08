9720744c931017faeb6dd47b6d04e159fc3ceaeb
Shakti Aggarwal <74964959+shakti2000@users.noreply.github.com>
2022-03-08 11:15:45 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (48.87%) - LeetHub

--- 1694-reformat-phone-number/1694-reformat-phone-number.cpp
+++ 1694-reformat-phone-number/1694-reformat-phone-number.cpp
@@ -1,39 +1,40 @@
 class Solution {
 public:
-    string reformatNumber(string number) {
-        int n = number.length();
-        string m="";
-        for(auto num: number){
-            if(num>='0' && num<='9') m+=num;
-        }
-        int s = m.size();
-        string res="";
-        int count=0;
-        int i=0, temp=s;
-        while(temp>4 && i<s){
-            res+=m[i];
-            res+=m[i+1];
-            res+=m[i+2];
-            res+='-';
-            i+=3;
-            temp-=3;
-        }
-        if(temp==4){
-            res+=m[i];
-            res+=m[i+1];
-            res+='-';
-            res+=m[i+2];
-            res+=m[i+3];
-        }
-        else if(temp==3){
-            res+=m[i];
-            res+=m[i+1];
-            res+=m[i+2];
-        }
-        else{
-            res+=m[i];
-            res+=m[i+1];
-        }
-        return res;
+    void print(string &s){
+        for(auto it:s)cout<<it<<",";
+        cout<<endl;
     }
+    string reformatNumber(string number) {
+        string s="";
+        for(int i=0;i<number.length();i++){
+            if(number[i]<'0'||number[i]>'9'){
+                continue;
+            }
+            s+=number[i];
+        }
+        //print(s);
+        int rem=s.length()%3;
+        if(rem==1)rem=4;
+        int temp=0;
+        string ans="";
+        while(s.length()>rem){
+            ans+=s[0];
+            s.erase(s.begin());
+            temp++;
+            if(s.length()!=0 && temp==3){
+               // cout<<s.length()<<endl;
+                temp=0;
+                ans+='-';
+            }
+        }
+        if(rem==4){
+            ans+=s[0];
+            ans+=s[1];
+            ans+='-';
+            ans+=s[2];
+            ans+=s[3];
+        }else{
+            for(auto it:s){ans+=it;}
+        }
+    return ans;}
 };
