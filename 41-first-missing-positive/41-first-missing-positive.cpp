1ac2ac33398e8600f7517f5d5db226c939c1a32b
Shakti Aggarwal <74964959+shakti2000@users.noreply.github.com>
2022-04-01 17:54:06 +0530

    Time: 183 ms (76.38%), Space: 82.8 MB (96.58%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.cpp
+++ 41-first-missing-positive/41-first-missing-positive.cpp
@@ -1,27 +1,10 @@
 class Solution {
 public:
-    int firstMissingPositive(vector<int>& x) {
-        unordered_set<int>nums;
-        for(auto it:x)nums.insert(it);
-        int ele=0;
-        int flag=0;
-        for(auto it:nums){
-            if(it==1)flag=1;
-            if(ele<it)ele=it;
-        }
-        if(ele==0||flag==0)return 1;
-        int comp=1;
-        cout<<ele<<",";
-        for(int i=1;i<=ele;i++){
-            if(nums.find(i)==nums.end())return i;
-        }
-        return ele+1;
-       //  for(int i=2;i<=ele;i++)comp=comp^i;
-       //  for(auto it:nums){
-       //      if(it<=0)continue;
-       //      comp=comp^it;
-       //  }
-       // if(comp==0)return ele+1;
-       //      return comp;
+    int firstMissingPositive(vector<int>& nums) {
+        int n = nums.size();
+        for (int i = 0; i < n; i++) if (nums[i] <= 0) nums[i] = n + 1;
+        for (int i = 0; i < n; i++) if (abs(nums[i]) <= n && nums[abs(nums[i]) - 1] > 0) nums[abs(nums[i]) - 1] *= -1;
+        for (int i = 0; i < n; i++) if (nums[i] > 0) return i + 1;
+        return n + 1;
     }
 };
