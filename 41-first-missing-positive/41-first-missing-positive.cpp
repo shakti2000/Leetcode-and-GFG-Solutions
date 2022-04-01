121d7d6d7fe0bf9465249613466990635219c2bc
Shakti Aggarwal <74964959+shakti2000@users.noreply.github.com>
2022-04-01 17:50:38 +0530

    Time: 412 ms (15.92%), Space: 132.8 MB (11.84%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.cpp
+++ 41-first-missing-positive/41-first-missing-positive.cpp
@@ -1,15 +1,27 @@
 class Solution {
 public:
-    int firstMissingPositive(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        int temp=1;
-        if(nums[0]>1)return 1;
-        for(int i=0;i<nums.size();i++){
-        if(temp == nums[i]) {
-                temp++;
-            }
-            else if (nums[i] > temp) break;
+    int firstMissingPositive(vector<int>& x) {
+        unordered_set<int>nums;
+        for(auto it:x)nums.insert(it);
+        int ele=0;
+        int flag=0;
+        for(auto it:nums){
+            if(it==1)flag=1;
+            if(ele<it)ele=it;
         }
-        return temp;
-}
+        if(ele==0||flag==0)return 1;
+        int comp=1;
+        cout<<ele<<",";
+        for(int i=1;i<=ele;i++){
+            if(nums.find(i)==nums.end())return i;
+        }
+        return ele+1;
+       //  for(int i=2;i<=ele;i++)comp=comp^i;
+       //  for(auto it:nums){
+       //      if(it<=0)continue;
+       //      comp=comp^it;
+       //  }
+       // if(comp==0)return ele+1;
+       //      return comp;
+    }
 };
