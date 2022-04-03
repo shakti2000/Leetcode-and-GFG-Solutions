67d2fda2a1f11173678e797786c76505d4d7b081
Shakti Aggarwal <74964959+shakti2000@users.noreply.github.com>
2022-04-03 19:36:11 +0530

    Time: 0 ms (100.00%), Space: 7.3 MB (40.66%) - LeetHub

--- 410-split-array-largest-sum/410-split-array-largest-sum.cpp
+++ 410-split-array-largest-sum/410-split-array-largest-sum.cpp
@@ -1,44 +1,30 @@
 class Solution {
 public:
-   bool helper(vector<int>&nums,int mid,int m){
+    bool helper(vector<int>& nums, int mid,int m){
+        int split=1;
         int cs=0;
-        int subs=1;
-        for(auto it:nums){
-            cs+=it;
-            if(it>=mid){
-                subs++;
-                cs=it;
-                if(subs>=m)return false;
+        for(int i=0;i<nums.size();i++){
+            cs+=nums[i];
+            if(cs>mid){
+                split++;
+                cs=nums[i];
             }
+        if(split>m)return false;
         }
-        return false;
+        return true;
     }
     int splitArray(vector<int>& nums, int m) {
-        int start =*max_element(nums.begin(),nums.end());
-        int end =accumulate(nums.begin(),nums.end(),0);
+        int start=*max_element(nums.begin(),nums.end());
+        int end=accumulate(nums.begin(),nums.end(),0);
         int mid,ans;
         while(start<=end){
             mid=start+(end-start)/2;
-            if(isPossible(nums,mid,m)){
+            if(helper(nums,mid,m)){
                 end=mid-1;
                 ans=mid;
             }else{
                 start=mid+1;
             }
         }
-        return ans;
-    }
-    
-    bool isPossible(vector<int>&n,int mid,int m){
-        int sum=0,count=1;
-        for(int i=0;i<n.size();i++){
-            sum+=n[i];
-            if(sum>mid){
-                sum=n[i];
-                count++;
-            }
-            if(count>m)return 0;
-        }
-        return 1;
-    }
+    return ans;}
 };
