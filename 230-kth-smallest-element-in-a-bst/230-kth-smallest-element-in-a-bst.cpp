94bcd2afdec85db9eee31bda855504ae2c968349
Shakti Aggarwal <74964959+shakti2000@users.noreply.github.com>
2022-04-18 17:30:46 +0530

    Time: 20 ms (74.44%), Space: 24.2 MB (60.91%) - LeetHub

--- 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.cpp
+++ 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.cpp
@@ -1,22 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    int num;
-    void help(TreeNode* root,int k,int&i)
-    {
-        if(root==NULL) return ;
-        
-        help(root->left,k,i);
-        if(i==k)
-        {
-          num = root->val;   
-        }
-        i++;
-        help(root->right,k,i);
+    void traverser(TreeNode*root,priority_queue<int>&pq,int &k){
+        if(root==NULL)return ;
+        pq.push(root->val);
+        if(pq.size()>k)pq.pop();
+        traverser(root->left,pq,k);
+        traverser(root->right,pq,k);
     }
-    int kthSmallest(TreeNode* root, int k) 
-    {
-        int i=1;
-        help(root,k,i);
-        return num;    
+    int kthSmallest(TreeNode* root, int k) {
+        priority_queue<int>pq;
+        traverser(root,pq,k);
+        return pq.top();
     }
 };
