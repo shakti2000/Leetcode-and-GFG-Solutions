b0f8d0a6bb0f64f08728d2fc98a946cc0906af0d
Shakti Aggarwal <74964959+shakti2000@users.noreply.github.com>
2022-05-24 12:55:38 +0530

    Time: 6 ms (13.70%), Space: 7.8 MB (26.08%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
+    int helper(vector<int>& dp,vector<int>& nums,int i){
+        if(i>=nums.size())return 0;
+        if(dp[i]!=-1)return dp[i];
+        int a=nums[i]+helper(dp,nums,i+2);
+        int b=nums[i]+helper(dp,nums,i+3);
+        return dp[i]=max(a,b);
+    }
     int rob(vector<int>& nums) {
-        vector<int>rob(nums.size());
         if(nums.size()==1)return nums[0];
         if(nums.size()==2)return max(nums[0],nums[1]);
-        int ma=0;
-        int ans;
-        rob[0]=nums[0];
-        rob[1]=nums[1];
-        rob[2]=nums[0]+nums[2];
-        ma=max(rob[2],rob[1]);
-        for(int i=3;i<nums.size();i++){
-            int ans=max(rob[i-2],rob[i-3])+nums[i];
-            rob[i]=ans;
-            if(ma<rob[i])ma=rob[i];
-        }
-    return ma;
+        vector<int>dp(nums.size()+1,-1);
+        int ans=helper(dp,nums,0);
+        int ans1=helper(dp,nums,1);
+        return max(ans,ans1);
     }
 };
