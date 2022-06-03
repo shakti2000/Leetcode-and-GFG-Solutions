fa21758f0979cffda2fba92844c8e832e0643e47
Shakti Aggarwal <74964959+shakti2000@users.noreply.github.com>
2022-06-03 17:08:36 +0530

    Time: 82 ms (83.44%), Space: 15.5 MB (32.05%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
-    void print(vector<int>&nums){
-        for(auto it:nums)cout<<it<<',';
-        cout<<endl;
-    }
-    int helper(vector<int>& nums,int amountl,vector<int>& dp){
-        if(amountl<0)return 100000009;
+    int helper(vector<int>& coins,int amountl,vector<int>& dp,int i){
+       // cout<<i<<","<<amountl<<endl;
+        if(amountl<0)return 100000008;
         if(amountl==0)return 0;
         if(dp[amountl]!=-1)return dp[amountl];
         int ans=100000009;
-        for(int i=0;i<nums.size();i++){
-            ans= min(ans,1+helper(nums,amountl-nums[i],dp));
+        for(int j=0;j<coins.size();j++){
+            ans=min(ans,1+helper(coins,amountl-coins[j],dp,j));
+           // if(ans!=100000009)break;
         }
-        return dp[amountl]=ans;
+       return dp[amountl]=ans;
     }
-    int coinChange(vector<int>& nums, int amount) {
-        sort(nums.rbegin(),nums.rend());
-       // print(nums);
+    int coinChange(vector<int>& coins, int amount) {
+        sort(coins.begin(),coins.end());
+      //  int ans=100000009;
         vector<int>dp(amount+1,-1);
-        int ans= helper(nums,amount,dp);
-        if(ans>=100000009)return -1;
+        int ans= helper(coins,amount,dp,0);
+        if(ans==100000009)return -1;
         else return ans;
     }
 };
