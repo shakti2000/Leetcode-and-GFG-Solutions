a2d11c691935f7495c2d980a4edf9519b993179e
Shakti Aggarwal <74964959+shakti2000@users.noreply.github.com>
2022-06-07 10:56:12 +0530

    Time: 0 ms (100.00%), Space: 9.1 MB (30.68%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -1,11 +1,35 @@
 class Solution {
 public:
+    void print(vector<int>nums){
+        for(auto it:nums)cout<<it<<",";
+        cout<<endl;
+    }
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int index=0;
-        for(int i=m;i<m+n;i++){
-            nums1[i]=nums2[index++];
+        if(n==0)return ;
+        if(m==0){
+            nums1=nums2;
+            return ;
         }
-        sort(nums1.begin(),nums1.end());
-        
+       // print(nums2);
+        int x=n+m;
+        x--;
+        n--;
+        m--;
+        while(n>=0&&x>=0){
+            
+              // cout<<m<<","<<n<<endl;
+            if(m>=0){
+            if(nums1[m]>nums2[n]){
+             //   cout<<nums1[m]<<">"<<nums2[n]<<endl;
+                swap(nums1[m--],nums1[x--]);
+            }else{
+              //  cout<<nums1[m]<<"<"<<nums2[n]<<endl;
+                nums1[x--]=nums2[n--];
+            }}else{
+                nums1[x--]=nums2[n--];
+            }
+            
+          //  print(nums1);
+        }
     }
 };
