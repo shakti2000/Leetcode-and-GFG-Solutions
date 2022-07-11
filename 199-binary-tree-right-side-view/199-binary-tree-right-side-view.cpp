409894c1e771481b05008960b6589ec48f9c076f
Shakti Aggarwal <74964959+shakti2000@users.noreply.github.com>
2022-07-11 10:29:48 +0530

    Time: 16 ms (5.32%), Space: 12.4 MB (6.59%) - LeetHub

--- 199-binary-tree-right-side-view/199-binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.cpp
@@ -11,19 +11,18 @@
  */
 class Solution {
 public:
-    void traverser(TreeNode* root,vector<int>&ans,int h,unordered_set<int>&s){
-        if(root==NULL)return;
-        if(s.find(h)==s.end()){
-            s.insert(h);
-            ans.push_back(root->val);
-        }
-        traverser(root->right,ans,h+1,s);
-        traverser(root->left,ans,h+1,s);
+    void traverser(TreeNode* root,int h,map<int,int>&mp){
+        if(root==NULL)return ;
+        if(mp.find(h)==mp.end())mp[h]=root->val;
+        traverser(root->right,h+1,mp);
+        traverser(root->left,h+1,mp);
     }
     vector<int> rightSideView(TreeNode* root) {
+       int h=0;
+        map<int,int>mp;
+        traverser(root,h,mp);
         vector<int>ans;
-        unordered_set<int>s;
-        traverser(root,ans,0,s);
+        for(auto it:mp)ans.push_back(it.second);
         return ans;
     }
 };
