8bec208eaea4d126b3e2bd5abcbe4e347a31b1ed
Shakti Aggarwal <74964959+shakti2000@users.noreply.github.com>
2022-07-12 23:42:26 +0530

    Time: 765 ms (26.53%), Space: 10.2 MB (47.49%) - LeetHub

--- 473-matchsticks-to-square/473-matchsticks-to-square.cpp
+++ 473-matchsticks-to-square/473-matchsticks-to-square.cpp
@@ -1,32 +1,30 @@
 class Solution {
-    bool dfs(vector<int> &sidesLength,const vector<int> &matches, int index, const int target) {
-        if (index == matches.size())
-            return sidesLength[0] == sidesLength[1] && sidesLength[1] == sidesLength[2] && sidesLength[2] == sidesLength[3];
-        for (int i = 0; i < 4; ++i) {
-            if (sidesLength[i] + matches[index] > target) // first
-                continue;
-            int j = i;
-            while (--j >= 0) // third
-                if (sidesLength[i] == sidesLength[j]) 
-                    break;
-            if (j != -1) continue;
-            sidesLength[i] += matches[index];
-            if (dfs(sidesLength, matches, index + 1, target))
-                return true;
-            sidesLength[i] -= matches[index];
-        }
-        return false;
-    }
 public:
-    bool makesquare(vector<int>& nums) {
-        if (nums.size() < 4) return false;
-        int sum = 0;
-        for (const int val: nums) {
-            sum += val;
+    bool helper(vector<int>& nums,vector<int>& side,int j,int sl){
+        if(j==nums.size())return true;
+        bool ans=false;
+        for(int i=0;i<4;i++){
+            if(side[i]+nums[j]<=sl){
+                side[i]+=nums[j];
+                ans|=helper(nums,side,j+1,sl);
+                if(ans==true)return true;
+                side[i]-=nums[j];
+            }
         }
-        if (sum % 4 != 0) return false;
-        sort(nums.begin(), nums.end(), [](const int &l, const int &r){return l > r;}); // second
-        vector<int> sidesLength(4, 0);
-        return dfs(sidesLength, nums, 0, sum / 4);
+        return ans;
+    }
+    bool makesquare(vector<int>& nums) {
+        int maxele=INT_MIN;
+        int sum=0;
+        for(auto it:nums){
+            sum+=it;
+            maxele=max(maxele,it);
+        }
+        if(sum%4!=0||maxele>(sum/4))return false;
+        sort(nums.rbegin(),nums.rend());
+        vector<int>side(4,0);
+        bool ans=false;
+        ans|=helper(nums,side,0,sum/4);
+        return ans;
     }
 };
