508ca98c7b50ccdbf28e4b40c20e53b86547d0e7
Shakti Aggarwal <74964959+shakti2000@users.noreply.github.com>
2022-07-15 07:47:58 +0530

    Time: 20 ms (86.81%), Space: 23.2 MB (89.29%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.cpp
+++ 695-max-area-of-island/695-max-area-of-island.cpp
@@ -1,15 +1,12 @@
 class Solution {
 public:
     int helper(vector<vector<int>>& grid,int i,int j){
-       // cout<<i<<","<<j<<endl;
         if(i>=grid.size()||j>=grid[0].size()||i<0||j<0)return 0;
         if(grid[i][j]==0)return 0;
         grid[i][j]=0;
         int ans=1;
         ans+=helper(grid,i+1,j);ans+=helper(grid,i-1,j);
         ans+=helper(grid,i,j-1);ans+=helper(grid,i,j+1);
-        //ans+=helper(grid,i+1,j+1);ans+=helper(grid,i-1,j-1);
-       // ans+=helper(grid,i-1,j+1);ans+=helper(grid,i+1,j-1);
         return ans;
     }
     int maxAreaOfIsland(vector<vector<int>>& grid) {
