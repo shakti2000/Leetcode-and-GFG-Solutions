376d0208db35c10bf81ed87daad3dfce9ef40da3
Shakti Aggarwal <74964959+shakti2000@users.noreply.github.com>
2022-07-26 10:16:00 +0530

    Time: 16 ms (89.68%), Space: 14.2 MB (57.81%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -9,32 +9,10 @@
  */
 class Solution {
 public:
-    bool present(TreeNode*p,TreeNode*root){
-        if(root==NULL)return false;
-        if(root==p)return true;
-        return present(p,root->left)||present(p,root->right);
-    }
-   
-     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-          while((present(p,root->right)&&present(q,root->right))||(present(p,root->left)&&present(q,root->left))){
-           if(present(p,root->right)&&present(q,root->right)){
-               root=root->right;
-           }
-        if(present(p,root->left)&&present(q,root->left)){
-        root=root->left;
-        }
-        
-       }
-    //    if(present(p,root->left)&&present(q,root->left)){
-    //        while(present(p,root->left)&&present(q,root->left)){
-    //            root=root->left;
-    //        }
-    //    }
-    //     if(present(p,root->right)&&present(q,root->right)){
-    //        while(present(p,root->right)&&present(q,root->right)){
-    //            root=root->right;
-    //        }
-    //    }
-        
- return root;   }
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+    if (!root || root == p || root == q) return root;
+    TreeNode* left = lowestCommonAncestor(root->left, p, q);
+    TreeNode* right = lowestCommonAncestor(root->right, p, q);
+    return !left ? right : !right ? left : root;
+}
 };
