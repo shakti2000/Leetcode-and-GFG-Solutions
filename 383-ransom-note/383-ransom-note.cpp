832f034c0b9b270d019bd9b7f5c9a9cfbd6dec0f
Shakti Aggarwal <74964959+shakti2000@users.noreply.github.com>
2022-08-25 19:23:35 +0530

    Time: 44 ms (20.98%), Space: 8.9 MB (39.49%) - LeetHub

--- 383-ransom-note/383-ransom-note.cpp
+++ 383-ransom-note/383-ransom-note.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
-        map<char,int>mp;
+        unordered_map<char,int>mp;
         for(auto it:magazine)mp[it]++;
         for(auto it:ransomNote){
             if(mp[it]==0)return false;
