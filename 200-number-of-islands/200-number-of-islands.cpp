058b9b5a49813e4f54a524c961fdc2ab741fd56f
Shakti Aggarwal <74964959+shakti2000@users.noreply.github.com>
2022-08-29 11:56:39 +0530

    Time: 43 ms (79.06%), Space: 12.3 MB (83.10%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,32 +1,27 @@
 class Solution {
 public:
-    void dfs(int i,int j,vector<vector<bool>>&visited, vector<vector<char>>&grid)
-   {
-        int n=grid[0].size(),m=grid.size(); //size of row and column
-       if(i<0 || j<0) return; 
-       if(i>=m || j>=n) return;
-       if(visited[i][j]) return;
-       visited[i][j]=true;
-       if(grid[i][j]=='0') return;
-       
-      
-       if(grid[i][j]=='1')
-       dfs(i+1,j,visited,grid);
-       dfs(i,j+1,visited,grid);
-       dfs(i-1,j,visited,grid);
-       dfs(i,j-1,visited,grid);
-   }
+    void calc(vector<vector<char>> &grid,int row,int col,int i,int j){
+        if(i>(row-1)||j>(col-1)||(i<0)||(j<0))return;
+        if(grid[i][j]=='1'){grid[i][j]='0';
+        calc(grid,row,col,i+1,j);
+                calc(grid,row,col,i,j+1);
+                calc(grid,row,col,i,j-1);
+                calc(grid,row,col,i-1,j);
+    }}
     int numIslands(vector<vector<char>>& grid) {
-        vector<vector<bool>>visited(grid.size(),vector<bool>(grid[0].size(),false));
-        int ans=0;
-            for(int i=0;i<grid.size();i++){
-            for(int j=0;j<grid[i].size();j++){
-                if(visited[i][j]==false&&grid[i][j]=='1'){
-                    ans++;
-                    dfs(i,j,visited,grid);
-                }
+        int row=grid.size();
+        int col=grid[0].size();
+        int count=0;
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                if(grid[i][j]=='1'){count++;
+                                    grid[i][j] = '0';
+                calc(grid,row,col,i+1,j);
+                calc(grid,row,col,i,j+1);
+                calc(grid,row,col,i,j-1);
+                calc(grid,row,col,i-1,j);}
             }
-                
         }
-  return ans;  }
+    return count;
+    }
 };
