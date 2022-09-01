24ee5d3e8c6b9a0527b5369baa158a6908a007de
Shakti Aggarwal <74964959+shakti2000@users.noreply.github.com>
2022-09-01 18:30:46 +0530

    Time: 38 ms (98.43%), Space: 18.9 MB (52.86%) - LeetHub

--- 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.cpp
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.cpp
@@ -1,58 +1,43 @@
 class Solution {
 public:
-    
-    /// main logic or trick for this problem : bahar se andar ki taraf jao
+    void dfs(int i,int j,vector<vector<int>>& temp,vector<vector<int>>& heights){
+     //  cout<<i<<","<<j<<endl;
+        if(temp[i][j]!=0)return;
+        temp[i][j]=1;
+        if(i-1>=0&&heights[i-1][j]>=heights[i][j]){
+            dfs(i-1,j,temp,heights);
+        }
+        if(j-1>=0&&heights[i][j-1]>=heights[i][j]){
+            dfs(i,j-1,temp,heights);
+        }
+        if(i+1<heights.size()&&heights[i+1][j]>=heights[i][j]){
+            dfs(i+1,j,temp,heights);
+        }
+        if(j+1<heights[0].size()&&heights[i][j+1]>=heights[i][j]){
+            dfs(i,j+1,temp,heights);
+        }
+        
+    }
     vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
-        
+        int n=heights.size(),m=heights[0].size();
+        vector<vector<int>>atlantic(n,vector<int>(m,0));
+        vector<vector<int>>pacific(n,vector<int>(m,0));
+        for(int i=0;i<n;i++){
+            dfs(i,0,pacific,heights);
+            dfs(i,m-1,atlantic,heights);
+        }
+        for(int j=0;j<m;j++){
+            dfs(0,j,pacific,heights);
+            dfs(n-1,j,atlantic,heights);
+        }
         vector<vector<int>>ans;
-        int m = heights.size();
-        int n = heights[0].size();
-        
-        vector<vector<bool>> pacific(m, vector<bool>(n));
-        vector<vector<bool>> atlantic(m, vector<bool>(n));
-        
-        for (int i = 0; i < m; i++) {
-            
-            dfs(heights, pacific, i, 0);
-            dfs(heights, atlantic, i, n-1);
-
-        }
-        
-        for (int j = 0; j < n; j++) {
-            
-            dfs(heights, pacific, 0, j);
-            dfs(heights, atlantic, m-1, j);
-        }
-
-        
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                
-                if (pacific[i][j] && atlantic[i][j]) // agar uss particular point se dono oceans mai jaa paa rahe hai
-                    ans.push_back({i,j});           // toh answer push kardo
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(atlantic[i][j]&&pacific[i][j]){
+                    ans.push_back({i,j});
+                }
             }
         }
-        return ans;
-    }
-    
-    void dfs(vector<vector<int>>& h, vector<vector<bool>>& vis, int i, int j) {
-        
-        int m = h.size();
-        int n = h[0].size();
-
-        vis[i][j] = true;
-        //up
-        if (i-1 >= 0 && vis[i-1][j] != true && h[i-1][j] >= h[i][j])
-            dfs(h, vis, i-1, j);
-        //down
-        if (i+1 < m && vis[i+1][j] != true && h[i+1][j] >= h[i][j])
-            dfs(h, vis, i+1, j);
-        //left
-        if (j-1 >= 0 && vis[i][j-1] != true && h[i][j-1] >= h[i][j])
-            dfs(h, vis, i, j-1);
-        //right
-        if (j+1 < n && vis[i][j+1] != true && h[i][j+1] >= h[i][j])
-            dfs(h, vis, i, j+1);
-
+            return ans;
     }
 };
