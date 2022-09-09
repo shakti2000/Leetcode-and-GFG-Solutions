015acb4cb992e103a86c44891e21a76422a2fd08
Shakti Aggarwal <74964959+shakti2000@users.noreply.github.com>
2022-09-09 10:27:58 +0530

    Time: 1647 ms (5.31%), Space: 125.2 MB (70.41%) - LeetHub

--- 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.cpp
+++ 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
-    static bool cmp(vector<int>&a,vector<int>&b){
-        if(a[0]!=b[0])return a[0]<b[0];
-        else return a[1]>b[1];
-    }
-    int numberOfWeakCharacters(vector<vector<int>>& properties) {
-        sort(properties.begin(),properties.end(),cmp);
-        int answer=0;
-        int mtn=INT_MIN;
-        for(int i = properties.size()-1; i>=0; i--){
-        if(mtn > properties[i][1]){
-            answer++;
-        }else{
-            mtn = max(mtn,properties[i][1]);
+    static bool shakti(vector<int>&a,vector<int>&b){
+        if(a[0]==b[0])return a[1]>b[1];
+        else{
+            return a[0]<b[0];
         }
     }
-    return answer;
+    int numberOfWeakCharacters(vector<vector<int>>& properties) {
+        sort(properties.begin(),properties.end(),shakti);
+        int mtn = INT_MIN;                            
+          int ans = 0;
+          for (int i = properties.size() - 1; i >= 0; i--)
+          {
+               if (properties[i][1] < mtn) 
+                    ans++;
+               mtn = max(mtn, properties[i][1]);
+          }
+          return ans;
     }
 };
